61406ac6d70ebfe745321329a8366ad9b45a5f9c
melikadamirchi <melikadamirchi2000@gmail.com>
2024-11-21 21:46:05 -0500

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./Comp1";
+import Comp1 from "./Comp1";
+import Comp2 from "./Comp2";
+import Comp3 from "./Comp3";
+import Comp4 from "./Comp4";
+import Comp5 from "./Comp5";
+import Comp6 from "./Comp6";
+import Comp7 from "./Comp7";
+import Comp8 from "./Comp8";
+import Comp9 from "./Comp9";
+import Comp10 from "./Comp10";
+import { Navbar } from "./pages/Navbar";
+import "./css/stylesComp3.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HashRouter>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Comp1" element={<Comp1 />}></Route>
+          <Route path="/Comp2" element={<Comp2 />}></Route>
+          <Route path="/Comp3" element={<Comp3 />}></Route>
+          <Route path="/Comp4" element={<Comp4 />}></Route>
+          <Route path="/Comp5" element={<Comp5 />}></Route>
+          <Route path="/Comp6" element={<Comp6 />}></Route>
+          <Route path="/Comp7" element={<Comp7 />}></Route>
+          <Route path="/Comp8" element={<Comp8 />}></Route>
+          <Route path="/Comp9" element={<Comp9 />}></Route>
+          <Route path="/Comp10" element={<Comp10 />}></Route>
+          <Route path="*" element={<h5>Nothing available</h5>}></Route>
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Comp2.jsx
@@ -0,0 +1,11 @@
+function Comp2() {
+  const names = ["meli1", " meli2"];
+
+  return (
+    <div>
+      <h1>Hi {names}</h1>
+    </div>
+  );
+}
+
+export default Comp2;
